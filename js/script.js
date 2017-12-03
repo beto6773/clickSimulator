d45b4821a7aaaa85f319921a64637213879012fd
manuPrg <mizquierdocolomina@gmail.com>
2017-12-04 00:33:18 +0100

    instalar Software. Random al minar. Cambio en café para adaptarlo al random de minar.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
-// Variables
-
+// Variables:
 var cartera = 0
 var tiempoRefresco = 10000;
 
+// Variable para las tarjetas gráficas
 var tarjetasGraficas = 1;
 var tarjetaGraficaRota = 0;
 var precioGrafica = 450;
 
+// Variables bitcoin
 var bitcoinMinar = 0.0000052;
+var bitcoinMax = 0.000015;
+var bitcoinMin = 0.0000052;
 var bitcoinFree;
 var bitcoin = 0;
 
-//Controlar la "trampa" de presionar Enter en lugar de hacer click
+// variable tiempo máximo y mínimo de actualizar()
+var tiempoMax = 10000;
+var tiempoMin = 5000;
 
+// Funcion para mostrar la informacion en pantalla. Se repite constantemente.
+function mostrarInfo() {
+	setInterval(function(){
+		bitcoinFree = Math.random() * (0.000011 - 0.0000052) + 0.0000052;
+		bitcoin = bitcoin + bitcoinFree;
+		tiempoRefresco = Math.random() * (tiempoMax - tiempoMin) + tiempoMin;
+		actualizar();
+	}, tiempoRefresco);
+}
+mostrarInfo();
+
+//Controlar la "trampa" de presionar Enter en lugar de hacer click
 function trampa(){
 	document.getElementById("minar").addEventListener("keydown", function(event){
 		if (event.keyCode == 13 || event.keyCode == 32) {
@@ -25,26 +42,17 @@ function trampa(){
 
 // Funcion para "minar". Aumenta los bitcoins en 0.01015
 function minar() {
+	bitcoinMinar = Math.random() * (bitcoinMax - bitcoinMin) + bitcoinMin;
 	bitcoin = bitcoin + bitcoinMinar;
-	tarjetaGraficaRota = Math.floor(Math.random() * (2000 - 1 + 1)) + 1;
-		if (tarjetaGraficaRota == 552 && tarjetasGraficas > 1) {
+	tarjetaGraficaRota = Math.floor(Math.random() * (2500 - 1 + 1)) + 1;
+		if (tarjetaGraficaRota == 550 && tarjetasGraficas > 1) {
 			alert ('Se te ha roto una tarjeta gráfica.')
 			tarjetasGraficas--;
-			bitcoinMinar = bitcoinMinar * 0.93;
+			bitcoinMinar = bitcoinMinar * 0.8;
 		}
 	actualizar();
 }
 
-// Funcion para mostrar la informacion en pantalla.
-function mostrarInfo() {
-	setInterval(function(){
-		bitcoinFree = Math.random() * (0.000011 - 0.0000052) + 0.0000052;
-		tiempoRefresco = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000;
-		bitcoin = bitcoin + bitcoinFree;
-		actualizar();
-	}, tiempoRefresco);
-}
-mostrarInfo();
 
 /* Actualiza la informacion de la pantalla, se ejecuta la funcion mostrarInfo() al cargar
 la pagina y esta llama a actualizar() cada
@@ -86,7 +94,7 @@ function comprarGrafica(x) {
 
 	cartera = cartera - precioGrafica;
 	tarjetasGraficas++
-	bitcoinMinar = (bitcoinMinar / 2) + 0.0000061;
+	bitcoinMinar = bitcoinMinar * 1.75;
 	document.getElementById('tarjetasGraficas').innerHTML = tarjetasGraficas;
 	actualizar();
 }
@@ -113,10 +121,15 @@ else {
 // funcion para comprar un "boost-pack". Se deshabilita por 30 segundos.
 var precioCafe = 2.50;
 
+// Variables para controlar el maximo y minimo actual.
+var maxActual;
+var minActual;
+
 function comprarCafe(){
 	if (tiempoEspera <= 30 && cartera > precioCafe) {
 		cartera = cartera - precioCafe;
-		minarActual = bitcoinMinar;
+		maxActual = bitcoinMax;
+		minActual = bitcoinMin;
 		cuentaAtras();
 	}
 	else {
@@ -125,7 +138,6 @@ function comprarCafe(){
 }
 
 var tiempoEspera = 30; // variable para controlar el tiempo de espera.
-var minarActual; // Variable para controlar el valor actual de la funcion de minar.
 
 function cuentaAtras() {
 	document.getElementById('comprarCafe').setAttribute('disabled', true);
@@ -135,10 +147,12 @@ function cuentaAtras() {
 			if ( tiempoEspera < 31 && tiempoEspera >= 25){
 
 				if ( tiempoEspera <= 25 ){
-					bitcoinMinar = minarActual;
+					bitcoinMax = maxActual;
+					bitcoinMin = minActual;
 				}
 				else {
-					bitcoinMinar = (bitcoinMinar * 3) / 1.2 ;
+					bitcoinMax = 0.00010;
+					bitcoinMin = 0.00005;
 				}
 				//bitcoinMinar = bitcoinMinar + (bitcoinMinar / 50);
 				document.getElementById('comprarCafe').innerHTML = "Comprar café (" + tiempoEspera + ")";
@@ -151,13 +165,69 @@ function cuentaAtras() {
 			}
 
 			else {
-				bitcoinMinar = minarActual;
+				bitcoinMax = maxActual;
+				bitcoinMin = minActual;
 				document.getElementById('comprarCafe').removeAttribute('disabled');
 				document.getElementById('comprarCafe').innerHTML = "Comprar café";
 				actualizar();
 				tiempoEspera = 30;
 			}
 		}, 1000);
+}
+
+//variables para la funcion comprarSoftware
+var x = 1; // controlar el progreso de la progressbar
+var comprar = 0; // controlar la confirmacion por parte del usuario
+var instalado = false; // controlar si el sofware está instalado
+
+function comprarSoftware() {
+
+	if (comprar == 0 && cartera >= 90){
+		document.getElementById('comprarSoftware').setAttribute('disabled',true);
+		comprar = confirm('¿Quieres comprar el sofware?');
+			if (instalado == true){
+				alert ('Ya tienes instalado este software');
+			}
+			else if (comprar == true){
+				cartera = cartera - 90;
+				actualizar();
+				comprarSoftware();
+			}
+			else if (comprar == false){
+				document.getElementById('comprarSoftware').removeAttribute('disabled');
+				actualizar();
+			}
+		}
+	else if (comprar > 0) {
+		if (x < 101){
+			document.getElementById("software").removeAttribute('hidden');
+			setTimeout(function(){
+			document.getElementById("softwareProgreso").setAttribute('style', 'width:' + x + '%');
+			document.getElementById('softwareProgreso').innerHTML = x + '%';
+			x++;
+			comprarSoftware();
+		}, Math.floor(Math.random() * (2000 - 200 + 1)) + 200);
+		}
+		else if (x > 100) {
+			if (tiempoMax == 10000 && tiempoMin == 5000){
+				tiempoMax = 3050;
+				tiempoMin = 1700;
+				tiempoRefresco = Math.random() * (tiempoMax - tiempoMin) + tiempoMin;
+				mostrarInfo();
+			}
+			comprar = 0;
+			x = 1;
+			document.getElementById("software").setAttribute('hidden', true);
+			document.getElementById("softwareProgreso").setAttribute('style', 'width:' + 0 + '%');
+			document.getElementById('comprarSoftware').setAttribute('title','Ya está instalado');
+			document.getElementById('comprarSoftware').setAttribute('class','btn btn-success');
+			document.getElementById('comprarSoftware').innerHTML = "¡Software.exe instalado!";
+			instalado = true;
+		}
+	}
+	else {
+		alert('No tienes dinero suficiente');
+	}
 }
 
 
